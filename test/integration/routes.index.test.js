3e0c19cadc50a637ed696a40ca600c1ddb1a0aa4
Ryan "archaen" Pando <vretha@gmail.com>
2017-09-27 19:36:25 -0600

    Add basic tests

--- test/integration/routes.index.test.js
+++ test/integration/routes.index.test.js
@@ -18,15 +18,14 @@ describe('routes : index', () => {
   });
 
   describe('GET /', () => {
-    it('should render the index', (done) => {
+    it('should return hello world', (done) => {
       chai.request(server)
       .get('/')
       .end((err, res) => {
         res.redirects.length.should.equal(0);
         res.status.should.equal(200);
-        res.type.should.equal('text/html');
-        res.text.should.contain('<h1>Welcome to Express!</h1>');
-        res.text.should.contain('<h2>The sum is 3</h2>');
+        res.type.should.equal('application/json');
+        res.body.message.should.contain('Hello World');
         done();
       });
     });
